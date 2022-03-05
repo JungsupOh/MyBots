6e1de7dafd25b7dba4cb38d131468e8aa3b60786
Jungsup <jungusp.oh@gmail.com>
2022-03-06 08:31:51 +0900

    Update DemodayTop10.py

--- DemodayTop10.py
+++ DemodayTop10.py
@@ -62,13 +62,13 @@ driver.get(url)
 print(url+ " Connected")
 
 li_list = driver.find_elements(by=By.XPATH, value="//li[@class='ranking_rankingItem__1a-1o']")
-
+print(li_list)
 links = []
 rank = 1
 for each in li_list:
     try:
         a_tags = each.find_elements(by=By.TAG_NAME,value='a')
-        
+        print(a_tags)
         #[0]:data [1]:image
         link=a_tags[0].get_attribute('href')
         bot.sendMessage(chat_id=167233193, text=str(rank)+') \n'+a_tags[0].accessible_name+'\n'+link)
