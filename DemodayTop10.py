cb62267ea673924528067b733be4135072da5292
Jungsup <jungusp.oh@gmail.com>
2022-03-06 12:07:18 +0900

    debug

--- DemodayTop10.py
+++ DemodayTop10.py
@@ -68,7 +68,7 @@ print("::Debug 12::")
 rank = 1
 for each in li_list:
     a_tags = each.find_elements(by=By.TAG_NAME,value='a')
-    print("debug11:: " + a_tags)
+    print("debug11:: " +str(rank) + ' --> '+ str(len(a_tags)))
     #[0]:data [1]:image
     link=a_tags[0].get_attribute('href')
     bot.sendMessage(chat_id=167233193, text=str(rank)+') \n'+a_tags[0].accessible_name+'\n'+link)
